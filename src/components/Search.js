c625698921902cf673cf2ea6a0963055c23394a0
MagdalenaMileto <maggiemileto@gmail.com>
2020-08-02 14:08:03 -0300

    Installing axios and make simple request to the wikipedia API.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import axios from "axios"
 
 //We will keep the exercise minimalist, doing all the search component in the same file,
 // we are not interested in the use of props at the moment, we only want to learn hooks.
@@ -6,7 +7,19 @@ const Search = () => {
     const [term, setTerm] = useState('');
 
     useEffect(() => {
-        console.log('I run after einitial render and after every change of data since last one')
+        const search = async () => {
+            await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                }
+            });
+        };
+        search();
+
     }, [term]);
 
     return (
